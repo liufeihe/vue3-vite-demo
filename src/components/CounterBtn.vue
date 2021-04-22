5370d652495b90d8671bf61070da71cc29878b5a
liufeihe <liufeihe@shizhuang-inc.com>
2021-04-22 23:49:13 +0800

    first blood

---
+++ src/components/CounterBtn.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="count++">count is: {{ count }}</button>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup: () => {
+    const count = ref(0)
+    return { count }
+  }
+})
+</script>
